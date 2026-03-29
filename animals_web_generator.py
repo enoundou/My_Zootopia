11a262c77c3eabee3148b080794f189cef42cfc2
Elianne Noundou <nouneliane@yahoo.fr>
2026-03-29 00:01:50 +0000

    add more fields to the animal records and verbessere HTML

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,32 +6,48 @@ def load_data(file_path):
     return json.load(handle)
 
 
-def output_animals(animals):
-    output =''
-    for animal in animals:
-        output += '<li class="cards__item">'
-        output += f"<div class='card__title'>{animal["name"]}</div>\n"
-        output += '<p class="card__text">'
-        output += f"<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n"
-        if "locations" in animal and len(animal["locations"]) > 0:
-            output += f"<strong>Location:</strong> {animal['locations'][0]}<br/>\n"
-        if 'characteristics' in animal and 'type' in animal['characteristics']:
-            output += f"<strong>Type:</strong> {animal['characteristics']['type']}<br/>\n"
-        output += '</p>'
-        output += '</li>'
-    return output
+def serialize_animal(animal_obj) :
+    # append information to each string
+    output_obj =''
+    output_obj += '<li class="cards__item">\n'
+    output_obj += f'<div class="card__title">{animal_obj["name"]}</div>\n'
+    output_obj += '<div class="card__text">'
+    output_obj += '<ul>'
+    if 'characteristics' in animal_obj and 'diet' in animal_obj['characteristics']:
+        output_obj += f"<li><strong>Diet:</strong> {animal_obj['characteristics']['diet']}</li>\n"
+    if "taxonomy" in animal_obj :
+        if "kingdom" in animal_obj["taxonomy"]:
+            output_obj += f"<li><strong>Kingdom:</strong> {animal_obj["taxonomy"]['kingdom']}</li>\n"
+        if "scientific_name" in animal_obj["taxonomy"]:
+            output_obj += f"<li><strong>Scientific name:</strong> {animal_obj["taxonomy"]['scientific_name']}</li>\n"
+    if "locations" in animal_obj and len(animal_obj["locations"]) > 0:
+        output_obj += f"<li><strong>Location:</strong> {animal_obj['locations'][0]}</li>\n"
+    if 'characteristics' in animal_obj :
+        if'type' in animal_obj['characteristics']:
+            output_obj += f"<li><strong>Type:</strong> {animal_obj['characteristics']['type']}</li>\n"
+        if'group' in animal_obj['characteristics']:
+            output_obj += f"<li><strong>Group:</strong> {animal_obj['characteristics']['group']}</li>\n"
+        if'skin_type' in animal_obj['characteristics']:
+            output_obj += f"<li><strong>Skin type:</strong> {animal_obj['characteristics']['skin_type']}</li>\n"
+    output_obj += '</ul>'
+    output_obj += '</div>'
+    output_obj += '</li>'
+
+    return output_obj
 
 
 def main():
-    animals_data = load_data('animals_data.json')
-    print(output_animals(animals_data))
+    data = load_data('animals_data.json')
+    output = ''
+    for animal_obj in data:
+        output += serialize_animal(animal_obj)
 
      # open and read file
     with open("animals_template.html", "r") as file:
         content = file.read()
 
     # replace __REPLACE_ANIMALS_INFO__
-    content = content.replace("__REPLACE_ANIMALS_INFO__", output_animals(animals_data))
+    content = content.replace("__REPLACE_ANIMALS_INFO__", output)
 
     # write back to file
     with open("animals_template.html", "w") as file:
